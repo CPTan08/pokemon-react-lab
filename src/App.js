ad7a8c72226897cc3316f30ab50a3c0afb702582
CP Tan <spin555@hotmail.com>
2021-08-17 10:20:13 +0800

    add map method  to populate all pokemon, add css style subtype button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@ import React from "react";
 import "./App.css";
 import pokemonData from "./pokemon/pokemon";
 
-const bulbasaur = pokemonData[0];
+//const bulbasaur = pokemonData[0];
+
+// function FilterPokemon(pokemonData) {
+//   for (var i = 0; i < pokemonData.length; i++) {
+//     var thisPoke = pokemonData[i];
+//   }
+//   return thisPoke;
+// }
 
 function PokemonCard({ pokemon }) {
   const { id, name, type, base } = pokemon;
   return (
-    <div>
-      <div>{name.english}</div>
+    <div className="card">
+      <img
+        className="images"
+        src={`${process.env.PUBLIC_URL}/pokemonImage/${id}.png`}
+        alt=""
+      />
+      <div className="container">
+        <h3>{name.english}</h3>
+      </div>
+      <div className="container">
+        <h3>{name.japanese}</h3>
+      </div>
+      <div className="container">
+        {type.map((subtype) => {
+          return (
+            <span className={`type ${subtype.toLowerCase()}`}>{subtype} </span>
+          );
+        })}
+      </div>
+
+      <br></br>
+      <div className="container">HP:{base.HP}</div>
+      <div className="container">Attack: {base.Attack}</div>
+      <div className="container">Defence: {base.Defence}</div>
+      <div className="container">SP Attack:{base.SpAttack}</div>
+      <div className="container">SP Defence:{base.SpDefence}</div>
+      <div className="container">Speed: {base.Speed}</div>
+      <br></br>
     </div>
   );
 }
@@ -16,7 +49,12 @@ function PokemonCard({ pokemon }) {
 function App() {
   return (
     <div className="App">
-      <PokemonCard pokemon={bulbasaur} />
+      {/* <PokemonCard pokemon={bulbasaur} /> */}
+      <div className="gallery">
+        {pokemonData.map((pokemon) => (
+          <PokemonCard pokemon={pokemon} key={pokemon.id} />
+        ))}
+      </div>
     </div>
   );
 }
